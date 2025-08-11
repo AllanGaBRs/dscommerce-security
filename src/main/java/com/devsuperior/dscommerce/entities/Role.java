6ed7d948aa02bd7a9716030eeb924922ce902cea
allangabr <allanmediogames@gmail.com>
2025-08-11 10:22:48 -0300

    feat: UserDetailsService

--- src/main/java/com/devsuperior/dscommerce/entities/Role.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Role.java
@@ -11,23 +11,23 @@ public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
     private String authority;
 
     public Role(){
 
     }
 
-    public Role(Integer id, String authority) {
+    public Role(Long id, String authority) {
         this.id = id;
         this.authority = authority;
     }
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
